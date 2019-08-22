a0281b78274bf1bdb075a7845787a0890d0384e7
hjelp200121 <johan.augsburg@gmail.com>
2019-08-22 14:25:33 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("Main");
+	}
+
+	public void settings() {
+		
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+}
