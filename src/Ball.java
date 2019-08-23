338d5063727c1870cceb756d62f9c1d23ea545f6
hjelp200121 <johan.augsburg@gmail.com>
2019-08-23 09:13:36 +0200

    Updating ball
    Adding an update function in ball

--- src/Ball.java
+++ src/Ball.java
@@ -1,10 +1,20 @@
+import processing.core.PApplet;
 
-public class Ball {
+public class Ball extends PApplet{
 	Vector position, velocity;
-	float radius = 25, rotation = 5;
+	public static float radius = 25;
+	public static float friction = 0.95f;
+	public float rotation = 5;
 	
 	Ball(Vector pos, Vector vel) {
 		position = pos;
 		velocity = vel;
+	}
+	
+	void update() {
+		velocity.mul(friction);
+		position.add(velocity);
+		
+		translate(position.x, position.y);
 	}
 }
