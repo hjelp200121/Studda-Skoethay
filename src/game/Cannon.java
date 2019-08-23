dd096295689ec72d0572e67ca72fa54793664b1c
Cornelius Sevald-Krause <cornelius.sevald@gmail.com>
2019-08-23 10:36:45 +0200

    Add cannon code
    In reference of issue #1

--- src/game/Cannon.java
+++ src/game/Cannon.java
@@ -1,9 +1,36 @@
 package game;
+import java.util.Deque;
+
 import processing.core.PImage;
 
 public class Cannon {
-	int cannonX, cannonY;
-	static String barrelPath = "data/cannonBarrel.png";
+	static final Vector barrelPivotOffset = new Vector(); // Find appropriate value
+	static GameManager gm = GameManager.gm;
+
 	static String basePath = "data/cannonBase.png";
-	PImage cannonBase, cannonBarrel;
+	static String barrelPath = "data/cannonBarrel.png";
+
+	public Vector position;
+	Deque<Ball> ammunition;
+	public double angle, minAngle, maxAngle, rotationSpeed;
+	public double power;
+
+	private PImage cannonBase, cannonBarrel;
+	/* The barrel pivot can also be used for the starting point when shooting balls. */
+	private Vector barrelPivot;
+	
+	public Cannon(Vector position, Deque<Ball> ammunition, double minAngle, double angle, double maxAngle,
+			double rotationSpeed, double power) {
+		this.position      = position;
+		this.ammunition    = ammunition;
+		this.angle         = angle;
+		this.minAngle      = minAngle;
+		this.maxAngle      = maxAngle;
+		this.rotationSpeed = rotationSpeed;
+		this.power         = power;
+		
+		this.cannonBase   = gm.loadImage(basePath);
+		this.cannonBarrel = gm.loadImage(barrelPath);
+		this.barrelPivot  = Vector.add(position, barrelPivotOffset);
+	}
 }
