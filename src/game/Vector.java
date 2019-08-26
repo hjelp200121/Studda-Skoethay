be3d0cf04955c8792ac06fed337ed0dc5b15dd63
hjelp200121 <johan.augsburg@gmail.com>
2019-08-26 10:35:38 +0200

    Adding documentation to the vector class
    in relation to issue #10

--- src/game/Vector.java
+++ src/game/Vector.java
@@ -50,42 +50,51 @@ public class Vector {
 		return new Vector(0, 0);
 	}
 	
+	/** adding 2 vectors getting a new one out */
 	public static Vector add (Vector v1, Vector v2) {
 		return new Vector(v1.x + v2.x, v1.y + v2.y);
 	}
 	
+	/** subtracting 2 vectors getting a new one out */
 	public static Vector sub (Vector v1, Vector v2) {
 		return new Vector(v1.x - v2.x, v1.y - v2.y);
 	}
 	
+	/** multiplying 2 vectors getting a new one out */
 	public static Vector mul (Vector v, float d) {
 		return new Vector(v.x * d, v.y * d);
 		}
 	
+	/** dividing 2 vectors getting a new one out */
 	public static Vector div (Vector v, float d) {
 		return new Vector(v.x / d, v.y / d);
 	}
-		
+	
+	/** adding the value of the second vector to the first */
 	public void add (Vector v) {
 		x += v.x;
 		y += v.y;
 	}
 	
+	/** subtracting the value of the second vector from the first */
 	public void sub (Vector v) {
 		x -= v.x;
 		y -= v.y;
 	}
 	
+	/** multiplying the value of the second vector to the first */
 	public void mul (float d) {
 		x *= d;
 		y *= d;
 	}
 	
+	/** dividing the value of the second vector from the first */
 	public void div (float d) {
 		x /= d;
 		y /= d;
 	}
 	
+	/** making a vector to be equal to another vector */
 	public void copy (Vector v) {
 		x = v.x;
 		y = v.y;
