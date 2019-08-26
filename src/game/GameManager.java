053c8505acfd903820033a2a8d59dc3ad75efead
Cornelius Sevald-Krause <cornelius.sevald@gmail.com>
2019-08-26 10:43:12 +0200

    Add documentation & minor fixes
    In reference of issue #10

--- src/game/GameManager.java
+++ src/game/GameManager.java
@@ -9,14 +9,17 @@ public class GameManager extends PApplet {
 	// Static singleton instance.
 	static GameManager gm = null;
 
+	/* The C&Bs */
 	Deque<Ball> balls;
 	Cannon C;
 
 	public void settings() {
 		System.out.println("Starting application...");
-		gm = this;
 		
+		/* Set the static singleton instance. */
+		gm = this;
 		size(1000, 1000);
+		/* Initialize the stack of ammunition. */
 		balls = new ArrayDeque<Ball>();
 	}
 	
@@ -24,23 +27,26 @@ public class GameManager extends PApplet {
 		frameRate(60);
 		smooth();
 		background(255);
-		C = new Cannon(new Vector(400,400),null,50,30,10,5,10);
-		
+		/* Initialize the cannon. */
+		C = new Cannon(new Vector(400,400),balls,50,30,10,5,10);
+		/* Load ammunition into the stack. */
 		balls.push(new Ball (new Vector(50,500), new Vector(5,0)));
 	}
 	
 	public void draw() {
 		background(255);
+		/* Draw the cannon and balls. */
 		C.show();
-		
 		for (Ball b : balls) {
 			b.update();
 		}
 	}
 	
 	public void keyPressed() {
+		/* When the user presses 'space',
+		 * add a new ball to the stack. */
 		if (key == ' ') {
-			balls.add(new Ball(new Vector(1,1), new Vector(2,2)));
+			balls.push(new Ball(new Vector(1,1), new Vector(2,2)));
 		}
 	}
 }
