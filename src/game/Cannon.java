e22bd40849f98e4fc7f8e3f23cf5b910e12d4c4f
Ben Shapiro <kimelskerungepigersbagdele@gmail.com>
2019-08-27 10:08:17 +0200

    Add decorative ball stack behind cannon
    In reference to issue #22.

--- src/game/Cannon.java
+++ src/game/Cannon.java
@@ -10,13 +10,14 @@ public class Cannon {
 
 	static String basePath = "data/cannonBase.png";
 	static String barrelPath = "data/cannonBarrel.png";
+	static String stackPath = "data/cannonBallStack.png";
 
 	public Vector position;
 	Deque<Ball> ammunition;
 	public double angle, minAngle, maxAngle, rotationSpeed;
 	public double power;
 
-	private PImage cannonBase, cannonBarrel;
+	private PImage cannonBase, cannonBarrel, ballStack;
 	/* The barrel pivot can also be used for the starting point when shooting balls. */
 	private Vector barrelPivot;
 	
@@ -32,15 +33,18 @@ public class Cannon {
 		
 		this.cannonBase   = gm.loadImage(basePath);
 		this.cannonBarrel = gm.loadImage(barrelPath);
+		this.ballStack = gm.loadImage(stackPath);
 		this.barrelPivot  = Vector.add(position, barrelPivotOffset);
 	}
 	
 	public void show() {
 		gm.imageMode(PApplet.CENTER);
-		cannonBarrel.resize(100,0);
-		cannonBase.resize(50,0);
-		gm.image(cannonBarrel,position.x,position.y - 22);
+		cannonBarrel.resize(160,0);
+		cannonBase.resize(70,0);
+		ballStack.resize(100,0);
+		gm.image(cannonBarrel,position.x,position.y - 30);
 		gm.image(cannonBase,position.x,position.y);
+		gm.image(ballStack,position.x - 80,position.y + 5);
 	}
 	
 	public void shoot() {
