hjelp200121/Studda-Skoethay

commit d36332c5fe98af81483cc9bdae6f18313789e5a7

Add command line option to skip emil
--skip-emil will skip the start screen.

In refrerence of issue #40

src/game/GameManager.java
@@ -50,6 +50,15 @@ public class GameManager extends PApplet {
 
 	/** size and initialisation */
 	public void settings() {
+		/* Process command line arguments. */
+		for (String arg : args) {
+			System.out.println(arg);
+			if (arg.equals("--skip-emil")) {
+				introScreen = false;
+				fadeIn = 0;
+			}
+		}
+		
 		System.out.println("Starting application...");
 		/* Set the static singleton instance. */
 		gm = this;
@@ -151,7 +160,7 @@ public class GameManager extends PApplet {
 		}
 		
 		/* Draw the fade in */
-		if (fadeIn > -5) {
+		if (fadeIn > 0) {
 			fill(255,255,255,fadeIn);
 			gm.rectMode(PApplet.CORNER);
 			rect(0,0,width,height);

commit 0442056efc24aa2f746843c3093cf662e04eeadc

Fix null pointer exception
Caused by d36332c

src/game/GameManager.java
@@ -51,11 +51,13 @@ public class GameManager extends PApplet {
 	/** size and initialisation */
 	public void settings() {
 		/* Process command line arguments. */
-		for (String arg : args) {
-			System.out.println(arg);
-			if (arg.equals("--skip-emil")) {
-				introScreen = false;
-				fadeIn = 0;
+		if (args != null) {
+			for (String arg : args) {
+				System.out.println(arg);
+				if (arg.equals("--skip-emil")) {
+					introScreen = false;
+					fadeIn = 0;
+				}
 			}
 		}
 		